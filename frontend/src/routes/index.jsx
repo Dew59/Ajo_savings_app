680ac182ff8081ca7c9e00105660bda37ef882e3
chidera-dominion <nwachukwudominion17@gmail.com>
2026-08-15 17:48:26 +0100

    url update

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -56,6 +56,10 @@ const router = createBrowserRouter([
     path: '*',
     element: <Navigate to="/dashboard" replace />,
   },
-]);
+],
+  {
+    basename: '/Ajo_savings_app',
+  }
+);
 
 export default router;
